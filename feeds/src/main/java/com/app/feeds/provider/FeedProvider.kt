c97894748117098bb5c9864d623817cf22fabc76
Megha Pabbi <megha.naxtre@gmail.com>
2021-11-01 20:21:26 +0530

    Search

--- feeds/src/main/java/com/app/feeds/provider/FeedProvider.kt
+++ feeds/src/main/java/com/app/feeds/provider/FeedProvider.kt
@@ -11,6 +11,7 @@ import org.koin.ext.scope
 
 class FeedProvider(private val feedRepository: FeedRepository) : AsyncProvider() {
 
+    var tempList: List<FeedItem> = emptyList()
 
     private val _feeds = MutableLiveData<List<FeedItem>>()
     val feeds: LiveData<List<FeedItem>> = _feeds
@@ -21,11 +22,12 @@ class FeedProvider(private val feedRepository: FeedRepository) : AsyncProvider()
     override fun executeCore(params: Parameters?) {
         setState(State.BUSY)
 
-         scope.launch {
-                feedRepository.loadFeed()
+        scope.launch {
+            feedRepository.loadFeed()
                 .onSuccess { feeds ->
                     setState(State.FINISHED)
                     _feeds.postValue(feeds)
+                    tempList = feeds
                 }
                 .onFailure {
                     setState(State.FAILED)
@@ -34,7 +36,7 @@ class FeedProvider(private val feedRepository: FeedRepository) : AsyncProvider()
         }
     }
 
-    fun bookMarkedItem(feedItem: FeedItem){
+    fun bookMarkedItem(feedItem: FeedItem) {
         scope.launch {
             _feeds.value?.toMutableList()?.let {
                 it[it.indexOf(feedItem)] = feedItem.copy(bookMarked = !feedItem.bookMarked)
@@ -43,11 +45,26 @@ class FeedProvider(private val feedRepository: FeedRepository) : AsyncProvider()
         }
     }
 
-    fun likeItem(feedItem: FeedItem){
+    fun likeItem(feedItem: FeedItem) {
         scope.launch {
             _feeds.value?.toMutableList()?.let {
                 it[it.indexOf(feedItem)] = feedItem.copy(liked = !feedItem.liked)
                 _feeds.postValue(it)
+            }
+        }
+    }
+
+    fun filterFeedFor(query: String?) {
+        scope.launch {
+            if (query.isNullOrBlank()) {
+                _feeds.postValue(tempList)
+            } else {
+                _feeds.postValue(tempList.filter {
+                    it.message.contains(
+                        query,
+                        true
+                    ) || it.from.name.contains(query, true)
+                })
             }
         }
     }
--- feeds/src/main/java/com/app/feeds/ui/feed/FeedViewModel.kt
+++ feeds/src/main/java/com/app/feeds/ui/feed/FeedViewModel.kt
@@ -1,6 +1,8 @@
 package com.app.feeds.ui.feed
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.app.feeds.data.domain.entity.FeedItem
 import com.app.feeds.provider.FeedProvider
@@ -11,15 +13,27 @@ class FeedViewModel(private val provider: FeedProvider) : ViewModel() {
     val feed: LiveData<List<FeedItem>>
         get() = provider.feeds
 
-    init {
-        provider.execute()
+    val searchFeed: MutableLiveData<String> = MutableLiveData()
+    private val queryObserver = Observer<String> {
+        provider.filterFeedFor(it)
     }
 
-    fun bookMarkedItem(feedItem: FeedItem){
+
+    init {
+        provider.execute()
+        searchFeed.observeForever(queryObserver)
+    }
+
+    fun bookMarkedItem(feedItem: FeedItem) {
         provider.bookMarkedItem(feedItem)
     }
 
-    fun likeItem(feedItem: FeedItem){
+    fun likeItem(feedItem: FeedItem) {
         provider.likeItem(feedItem)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        searchFeed.observeForever(queryObserver)
     }
 }
